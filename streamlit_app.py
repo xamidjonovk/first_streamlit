876d4291b579c7de3e6b0e93cd9ea027d33c55db
Komiljon <77206354+xamidjonovk@users.noreply.github.com>
2022-06-22 11:14:13 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,14 @@ import pandas as pd
 import streamlit as st
 
 """
-# My first streamlit app 
+# O'z spiralingizni yarating! 
 Komiljon Xamidjonov
 """
 
 
 
-total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+total_points = st.slider("Spiraldagi nuqtalar soni:", 1, 5000, 2000)
+num_turns = st.slider("Spiral halqalari soin:", 1, 100, 9)
 
 Point = namedtuple('Point', 'x y')
 data = []
